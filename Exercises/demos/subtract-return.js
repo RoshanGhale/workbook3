75d713af9ef5951946433915b82c975709bcac4b
roshanghale <rghale0110@gmail.com>
2024-04-30 13:08:01 -0400

    subtract return

---
+++ Exercises/demos/subtract-return.js
@@ -0,0 +1,17 @@
+function sub(num1, num2) {
+  var result = num1 - num2;
+  return result;
+}
+var resultCaught = sub(8, 5);
+console.log(resultCaught);
+//or
+console.log(sub(8, 5));
+console.log();
+function multiply(x, y) {
+  var result = x * y;
+  return result;
+}
+var resultCaught = multiply(2, 8);
+console.log(resultCaught);
+//or
+console.log(multiply(2, 8));
