88866ff95bc71252882537e1bd11c84b6b94cc91
roshanghale <rghale0110@gmail.com>
2024-05-01 14:44:22 -0400

    birthday

---
+++ Exercises/exercises/dates/date_to string.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let d = new Date();
+console.log(d.toString());
+
+console.log(d.toDateString());
+
+console.log(d.toUTCString());
+
+console.log(d.toLocaleString());
+
+//Exercise 2
---
+++ Exercises/exercises/dates/more_date_conversions.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let birthday = new Date(2001, 8, 12);
+console.log(birthday.toLocaleString());
+
+let momsBirthday = new Date(1980, 11, 23);
+console.log(momsBirthday.toLocaleString());
