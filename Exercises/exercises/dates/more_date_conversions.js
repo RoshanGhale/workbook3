f65913a4d2f1acdbee31ab64c3ec782ab6b91ce5
roshanghale <rghale0110@gmail.com>
2024-05-01 14:50:19 -0400

    add dads birthday

--- Exercises/exercises/dates/more_date_conversions.js
+++ Exercises/exercises/dates/more_date_conversions.js
@@ -5,3 +5,6 @@ console.log(birthday.toLocaleString());
 
 let momsBirthday = new Date(1980, 11, 23);
 console.log(momsBirthday.toLocaleString());
+
+let dadsBirthday = new Date(1975, 5, 20);
+console.log(dadsBirthday.toLocaleString());
